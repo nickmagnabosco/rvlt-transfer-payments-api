e5df5b030cb5fef0a80e85526656d22fb7115d51
nicola-magnabosco <nicola.magnabosco@publicis-sapient.comi>
2019-07-12 18:22:18 +0100

    Add initial add design: - add REST adapter - add repository interfaces - add application bootstrap initializer - add main domain objects and dtos

--- transferapi/src/main/java/revolut/transfer/TransferApiApplication.java
+++ transferapi/src/main/java/revolut/transfer/TransferApiApplication.java
@@ -1,8 +1,5 @@
 package revolut.transfer;
 
-import revolut.transfer.integration.adapters.TransferAdapter;
-
-import java.util.ResourceBundle;
 
 import static spark.Spark.awaitInitialization;
 import static spark.Spark.port;
@@ -12,7 +9,7 @@ public class TransferApiApplication {
     public static void main(String[] args) {
 //        port(ResourceBundle.getBundle("configuration/local.properties").getString("application.port"));
         port(8080);
-        new TransferAdapter().initialize();
-//        awaitInitialization();
+        new ApplicationBootstrapInitializer().initialize();
+        awaitInitialization();
     }
 }
