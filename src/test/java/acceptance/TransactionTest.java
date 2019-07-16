0ea8e4f0f32064c651cc996bd89a1e1aa774da14
nicola-magnabosco <nicola.magnabosco@publicis-sapient.comi>
2019-07-16 20:53:36 +0100

    Add test for deposit

--- src/test/java/acceptance/TransactionTest.java
+++ src/test/java/acceptance/TransactionTest.java
@@ -14,11 +14,12 @@ import java.math.BigDecimal;
 import static acceptance.TestUtility.givenAccount;
 import static acceptance.TestUtility.givenAccountHolder;
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class TransactionTest extends ComponentTest {
     @Test
-    public void createTransaction() {
+    public void createDepositTransaction() {
         AccountHolderDetails accountHolderDetails = givenAccountHolder(new CreateAccountHolder(
                 "MR",
                 "Clint",
@@ -45,5 +46,97 @@ public class TransactionTest extends ComponentTest {
         assertThat(transaction.getAmount()).isEqualTo(new MonetaryAmount(BigDecimal.valueOf(10), "GBP"));
         assertThat(transaction.getType()).isEqualTo("DEPOSIT");
         assertThat(transaction.getStatus()).isEqualTo("IN_PROGRESS");
+
+        Account getAccount = when()
+                .get(getFullUrl("/accountHolders/" + accountHolderDetails.getId() + "/accounts/" + account.getId()))
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(Account.class);
+
+        assertThat(getAccount.getBalance()).isEqualTo(new MonetaryAmount(BigDecimal.valueOf(10), "GBP"));
+    }
+
+    @Test
+    public void createMultipleDepositTransaction_sameRequestId_400() {
+        AccountHolderDetails accountHolderDetails = givenAccountHolder(new CreateAccountHolder(
+                "MR",
+                "Clint",
+                "Eastwood",
+                "myemail@address.com",
+                "UK"));
+
+        Account account = givenAccount(accountHolderDetails.getId(), new CreateAccountCommand("UK"));
+
+        CreateDeposit createDeposit = new CreateDeposit("req234", account.getId(), new MonetaryAmount(BigDecimal.valueOf(10), "GBP"));
+        given()
+                .body(createDeposit)
+                .when()
+                .post(getFullUrl("/accountHolders/" + accountHolderDetails.getId() + "/accounts/" + account.getId() + "/deposit"))
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Transaction.class);
+
+        given()
+                .body(createDeposit)
+                .when()
+                .post(getFullUrl("/accountHolders/" + accountHolderDetails.getId() + "/accounts/" + account.getId() + "/deposit"))
+                .then()
+                .statusCode(400);
+
+        Account getAccount = when()
+                .get(getFullUrl("/accountHolders/" + accountHolderDetails.getId() + "/accounts/" + account.getId()))
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(Account.class);
+
+        assertThat(getAccount.getBalance()).isEqualTo(new MonetaryAmount(BigDecimal.valueOf(10), "GBP"));
+    }
+
+    @Test
+    public void createMultipleDepositTransaction_differentRequestId_200() {
+        AccountHolderDetails accountHolderDetails = givenAccountHolder(new CreateAccountHolder(
+                "MR",
+                "Clint",
+                "Eastwood",
+                "myemail@address.com",
+                "UK"));
+
+        Account account = givenAccount(accountHolderDetails.getId(), new CreateAccountCommand("UK"));
+
+        CreateDeposit createDeposit = new CreateDeposit("req345", account.getId(), new MonetaryAmount(BigDecimal.valueOf(10), "GBP"));
+        given()
+                .body(createDeposit)
+                .when()
+                .post(getFullUrl("/accountHolders/" + accountHolderDetails.getId() + "/accounts/" + account.getId() + "/deposit"))
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Transaction.class);
+
+        CreateDeposit createDeposit2 = new CreateDeposit("req456", account.getId(), new MonetaryAmount(BigDecimal.valueOf(10), "GBP"));
+
+        given()
+                .body(createDeposit2)
+                .when()
+                .post(getFullUrl("/accountHolders/" + accountHolderDetails.getId() + "/accounts/" + account.getId() + "/deposit"))
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Transaction.class);
+
+        Account getAccount = when()
+                .get(getFullUrl("/accountHolders/" + accountHolderDetails.getId() + "/accounts/" + account.getId()))
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(Account.class);
+
+        assertThat(getAccount.getBalance()).isEqualTo(new MonetaryAmount(BigDecimal.valueOf(20), "GBP"));
     }
 }
